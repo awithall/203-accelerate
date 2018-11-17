a932673d26a27903ea71793e177b99d281ea99f8
FracturedFocus <ashleigh.withalln@gmail.com>
2018-11-17 15:23:35 -0800

    siingle case studies template added

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -9,13 +9,47 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
+<div id="primary" class="site-content sidebar">
+    <div class="main-content" role="main">
+            
+    <?php while ( have_posts() ) : the_post();	
+	$services = get_field('services');
+    $client = get_field('client');
+	$link = get_field('site_link'); 
+    $image_1 = get_field('image_1');  
+    $image_2 = get_field('image_2');  
+    $image_3 = get_field('image_3'); 
+    $size = "full"; ?>    
+ 
+	<article class="case-study">
+		<aside class="case-study-sidebar">
+			<h2><?php the_title(); ?></h2>
+			<h6><?php echo $services; ?></h6>
+            <h5><?php echo $client; ?></h5>
+ 
+			<?php the_content(); ?>
+					
+			<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site ›</a></p>
+		</aside>
+        
+        <div class="case-study-images">
+            <?php if($image_1) { ?>
+	           <? echo wp_get_attachment_image( $image_1, $size ); ?>
+            <?php } ?>
+            <?php if($image_2) { ?>
+	           <? echo wp_get_attachment_image( $image_2, $size ); ?>
+            <?php } ?>
+            <?php if($image_3) { ?>
+                <? echo wp_get_attachment_image( $image_3, $size ); ?>
+            <?php } ?>
+        </div>
+	</article>
+<?php endwhile; ?>
+    </div><!-- .main-content -->
 
-	</div><!-- #primary -->
+</div><!-- #primary -->
+<div id="navigation" class="container">
+	<div class="left"><a href="<?php echo site_url('/blog/') ?>">&larr;<span>Back to work</span></a></div>
+</div>
 
 <?php get_footer(); ?>
