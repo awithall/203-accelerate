8f7efbbc87df21e812de180e75b00efad14f54c9
FracturedFocus <ashleigh.withalln@gmail.com>
2018-12-26 21:35:59 -0800

    homepage case studies update

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -11,27 +11,27 @@ get_header(); ?>
 
 <div id="primary" class="site-content sidebar">
     <div class="main-content" role="main">
-            
-    <?php while ( have_posts() ) : the_post();	
-	$services = get_field('services');
+
+    <?php while ( have_posts() ) : the_post();
+	  $services = get_field('services');
     $client = get_field('client');
-	$link = get_field('site_link'); 
-    $image_1 = get_field('image_1');  
-    $image_2 = get_field('image_2');  
-    $image_3 = get_field('image_3'); 
-    $size = "full"; ?>    
- 
+	  $link = get_field('site_link');
+    $image_1 = get_field('image_1');
+    $image_2 = get_field('image_2');
+    $image_3 = get_field('image_3');
+    $size = "full"; ?>
+
 	<article class="case-study">
 		<aside class="case-study-sidebar">
 			<h2><?php the_title(); ?></h2>
 			<h6><?php echo $services; ?></h6>
             <h5><?php echo $client; ?></h5>
- 
+
 			<?php the_content(); ?>
-					
+
 			<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site ›</a></p>
 		</aside>
-        
+
         <div class="case-study-images">
             <?php if($image_1) { ?>
 	           <? echo wp_get_attachment_image( $image_1, $size ); ?>
@@ -49,7 +49,7 @@ get_header(); ?>
 
 </div><!-- #primary -->
 <div id="navigation" class="container">
-	<div class="left"><a href="<?php echo site_url('/blog/') ?>">&larr;<span>Back to work</span></a></div>
+	<div class="left"><a href="<?php echo site_url('/case-studies/') ?>">&larr;<span>Back to work</span></a></div>
 </div>
 
 <?php get_footer(); ?>
