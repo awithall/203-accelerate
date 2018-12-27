e58cd0e5470dffe208db9878d3c1ee072f3af3ed
FracturedFocus <ashleigh.withalln@gmail.com>
2018-12-26 17:19:38 -0800

    custome fileds plugin update, Homepage update

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -21,15 +21,39 @@ get_header(); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
-	<section class="recent-posts">
+
+<section class="featured-work">
+    <div class="site-content">
+
+        <h3>FEATURED WORK</h3>
+		<ul class="homepage_featured_work">
+ 			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+  		        <?php while ( have_posts() ) : the_post();
+                  $image_1 = get_field("image_1");
+									$size = "medium";
+            	?>
+								<li>
+									<figure><a href="<?php the_permalink (); ?>">
+										<?php echo wp_get_attachment_image($image_1, $size); ?>
+									</a></figure>
+									 <h3><a href="<?php the_permalink (); ?>"><?php the_title(); ?></a></h3>
+								</li>
+							<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</ul>
+    </div>
+</section>
+
+
+<section class="recent-posts">
  		<div class="site-content">
     		<div class="blog-post">
  				<h4>From the Blog</h4>
  					<?php query_posts('posts_per_page=1'); ?>
   						<?php while ( have_posts() ) : the_post(); ?>
 							<h3><?php the_title(); ?></h3>
-       						<?php the_excerpt(); ?> 
-  						<?php endwhile; ?> 
+       						<?php the_excerpt(); ?>
+  						<?php endwhile; ?>
 					<?php wp_reset_query(); ?>
   			</div>
  		</div>
